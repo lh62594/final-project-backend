7c764911bd84142d3a509d14a4e2f9aa93c6432d
Linya Hu <lh62594@yahoo.com>
2019-04-10 18:05:08 -0400

    create account working with main dashboard

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,6 +5,16 @@ class Api::V1::UsersController < ApplicationController
     render json: @users
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      jwt = encode_token({user_id: @user.id})
+      Dashboard.create(user_id: @user.id, name: "main", newsfeed: true)
+      render json: {user: @user, jwt: jwt}
+    else
+      render json: {errors: user.errors.full_messages}
+    end
+  end
 
   private
 
@@ -13,7 +23,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def user_params
-    params.permit(:first_name, :last_name, :username, :password)
+    params.permit(:first_name, :last_name, :email, :username, :password)
   end
 
 end
