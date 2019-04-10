36845aec759647ac13c3e601ccb51da4a36d3d56
Linya Hu <lh62594@yahoo.com>
2019-04-10 16:33:53 -0400

    auth implemented

--- Gemfile
+++ Gemfile
@@ -12,6 +12,10 @@ gem 'stock_quote'
 
 gem 'rb-readline'
 
+gem 'jwt'
+
+gem 'dotenv-rails', groups: [:development, :test]
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, :equities, :dashboards, :equity_dashboards
+
+      # route to login
+      post "/login", to: "auth#login"
+
+      # login on refresh
+      get '/auto_login', to: "auth#auto_login"
+      
     end
   end
 end
