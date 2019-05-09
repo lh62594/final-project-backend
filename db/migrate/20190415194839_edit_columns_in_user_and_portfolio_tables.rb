cf3c69c269720aea0d4ca32afe6f52321de0e271
Linya Hu <lh62594@yahoo.com>
2019-05-09 12:48:53 -0400

    final

--- db/migrate/20190415194839_edit_columns_in_user_and_portfolio_tables.rb
+++ db/migrate/20190415194839_edit_columns_in_user_and_portfolio_tables.rb
@@ -3,6 +3,6 @@ class EditColumnsInUserAndPortfolioTables < ActiveRecord::Migration[5.2]
     add_column :portfolios, :account_balance, :decimal
 
     remove_column :users, :account_balance
-    
+
   end
 end
